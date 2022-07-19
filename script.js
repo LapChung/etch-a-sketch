e5ae843fa87b7e5410e272e34f2fc51a8009be31
Lap Chung <lapchung12@gmail.com>
2022-07-19 15:14:39 -0400

    Added color changing when clicked/hold

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let slideValue = document.querySelector("#slider");
 let dimension = document.querySelector("#dimension");
+let gridContainer = document.querySelector(".grid-container");
 dimension.textContent = `${slideValue.value} x ${slideValue.value}`;
 
 slideValue.addEventListener("input", (event) => {
@@ -7,18 +8,28 @@ slideValue.addEventListener("input", (event) => {
   dimension.textContent = `${slideValue.value} x ${slideValue.value}`;
 });
 
+let isMouseDown = false;
 const createGrid = () => {
-  let gridContainer = document.querySelector(".grid-container");
-  let gridSize = Math.pow(slideValue.value, 2);
   gridContainer.textContent = "";
 
   gridContainer.style.gridTemplateColumns = `repeat(${slideValue.value}, 1fr)`;
   gridContainer.style.gridTemplateRows = `repeat(${slideValue.value}, 1fr)`;
 
-  for (let i = 0; i < gridSize; i++) {
+  for (let i = 0; i < slideValue.value * slideValue.value; i++) {
     let gridBox = document.createElement("div");
     gridBox.classList.add("grid-box");
     gridContainer.append(gridBox);
+
+    gridBox.onmousedown = (event) => {
+      isMouseDown = true;
+      event.target.style.backgroundColor = "red";
+    };
+    gridBox.onmouseup = () => (isMouseDown = false);
+    gridBox.addEventListener("mouseover", (event) => {
+      if (isMouseDown) {
+        event.target.style.backgroundColor = "red";
+      }
+    });
   }
 };
 
