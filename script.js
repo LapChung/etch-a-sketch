efa14033e03d78a51b114716b4896939b9bbd9d9
Lap Chung <lapchung12@gmail.com>
2022-07-21 16:13:09 -0400

    Added current active button

--- script.js
+++ script.js
@@ -8,7 +8,13 @@ slideValue.addEventListener("input", (event) => {
   dimension.textContent = `${slideValue.value} x ${slideValue.value}`;
 });
 
+// Global variable to track mouse
 let isMouseDown = false;
+document.body.onmousedown = () => (isMouseDown = true);
+document.body.onmouseup = () => (isMouseDown = false);
+
+let colorPicker = document.querySelector("#colorPicker");
+
 const createGrid = () => {
   gridContainer.textContent = "";
 
@@ -20,17 +26,63 @@ const createGrid = () => {
     gridBox.classList.add("grid-box");
     gridContainer.append(gridBox);
 
-    gridBox.onmousedown = (event) => {
-      isMouseDown = true;
-      event.target.style.backgroundColor = "red";
-    };
-    gridBox.onmouseup = () => (isMouseDown = false);
+    gridBox.addEventListener("mousedown", (event) => {
+      event.target.style.backgroundColor = colorPicker.value;
+    });
     gridBox.addEventListener("mouseover", (event) => {
       if (isMouseDown) {
-        event.target.style.backgroundColor = "red";
+        event.target.style.backgroundColor = colorPicker.value;
       }
     });
   }
 };
 
-createGrid();
+let colorBtn = document.querySelector("#colorBtn");
+let rainbowBtn = document.querySelector("#rainbowBtn");
+let eraseBtn = document.querySelector("#eraseBtn");
+let clearBtn = document.querySelector("#clearBtn");
+let currentMode = "color";
+
+const setNewMode = (newMode) => {
+  activeButton(newMode);
+  currentMode = newMode;
+};
+
+colorBtn.onclick = () => setNewMode("color");
+rainbowBtn.onclick = () => setNewMode("rainbow");
+eraseBtn.onclick = () => setNewMode("erase");
+
+const activeButton = (newMode) => {
+  switch (currentMode) {
+    case "color":
+      colorBtn.classList.remove("active");
+      break;
+    case "rainbow":
+      rainbowBtn.classList.remove("active");
+      break;
+    case "erase":
+      eraseBtn.classList.remove("active");
+      break;
+    default:
+      text = "No button found";
+  }
+
+  switch (newMode) {
+    case "color":
+      colorBtn.classList.add("active");
+      break;
+    case "rainbow":
+      rainbowBtn.classList.add("active");
+      break;
+    case "erase":
+      eraseBtn.classList.add("active");
+      break;
+    default:
+      text = "No button found";
+  }
+};
+
+window.onload = () => {
+  createGrid();
+  activeButton(currentMode);
+};
